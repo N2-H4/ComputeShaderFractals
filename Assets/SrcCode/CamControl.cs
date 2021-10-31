d1cf0cc12ba42a9ba73953bd4d231a217a8c7568
N2 H4 <n.nawrocki@onet.pl>
2021-10-31 14:28:35 +0100

    Main Menu Added You can now choose between Mandelbrot set, Julia Set, Mandelbulb, 3D Sierpinski triangle

---
+++ Assets/SrcCode/CamControl.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CamControl : MonoBehaviour
+{
+    Transform t;
+    void Start()
+    {
+        t = GetComponent<Transform>();
+    }
+
+    void Update()
+    {
+        if(Input.GetKey(KeyCode.W))
+        {
+            t.position += t.forward * Time.deltaTime * 0.4f;
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            t.position -= t.forward * Time.deltaTime * 0.4f;
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            t.position += t.right * Time.deltaTime * 0.4f;
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            t.position -= t.right * Time.deltaTime * 0.4f;
+        }
+        if (Input.GetKey(KeyCode.Space))
+        {
+            t.position += t.up * Time.deltaTime * 0.4f;
+        }
+        if(Input.GetKey(KeyCode.LeftShift))
+        {
+            t.position -= t.up * Time.deltaTime * 0.4f;
+        }
+    }
+}
---
+++ Assets/SrcCode/SceneChanging.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneChanging : MonoBehaviour
+{
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+
+    public void toMandelbrot()
+    {
+        SceneManager.LoadScene("Mandelbrot");
+    }
+
+    public void toJulia()
+    {
+        SceneManager.LoadScene("Julia");
+    }
+
+    public void toMandelbulb()
+    {
+        SceneManager.LoadScene("Mandelbulb");
+    }
+
+    public void toSierpinski()
+    {
+        SceneManager.LoadScene("Sierpinski");
+    }
+}
